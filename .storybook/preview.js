93986d013a056efcb7bd3ea11f58207a95124a49
Yusuke <y-maeda@recus-groove.com>
2022-12-23 11:16:22 +0900

    a11y migration to v6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
-import { addDecorator } from '@storybook/html';
-import { withA11y } from '@storybook/addon-a11y'; //全てのコンポーネントで反映させるため、ここでimport
+// import { addDecorator } from '@storybook/html';
+// import { withA11y } from '@storybook/addon-a11y'; //全てのコンポーネントで反映させるため、ここでimport
 // import '../src/scss/css/bundle.scss'; // グローバルスタイル
 
-addDecorator(withA11y);
+// addDecorator(withA11y);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -11,5 +11,11 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  a11y: {
+    element: '#storybook-root',
+    config: {},
+    options: {},
+    manual: true,
   },
 }
