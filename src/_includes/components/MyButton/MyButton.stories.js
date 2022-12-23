b0bd7700159d2e5e26620ad717622f96dc8b0af3
Yusuke <y-maeda@recus-groove.com>
2022-12-23 11:22:02 +0900

    add readme by @storybook/addon-storysource

--- src/_includes/components/MyButton/MyButton.stories.js
+++ src/_includes/components/MyButton/MyButton.stories.js
@@ -1,6 +1,25 @@
 import MyButton from './MyButton.ejs'; // コンポーネントを読み込む
 import '../../../styles/components/MyButton.scss';
 
+const readme = ` // @storybook/addon-storysource用の記述
+## Props
+- label
+  - 初期値: ''
+- type
+  - 初期値: 'button'
+- disabled
+  - 初期値: false
+- modefireClass
+  - modefire用のクラス付与
+  - 初期値: ''
+- jsClass
+  - JavaScript用のクラス付与
+  - 初期値: ''
+
+## 関連コンポーネント
+なし
+`;
+
 export default {
   title: 'MyButton', // storybookでリストに表示されるタイトル名
   component: MyButton, // importしたコンポーネント
@@ -13,6 +32,9 @@ export default {
     disabled: { disable: false },
     modefireClass: { control: 'text' },
     jsClass: { control: 'text' },
+  },
+  parameters: {
+    notes: readme // readme読み込み
   },
 };
 
