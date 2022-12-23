b2335fab1167f9602c4ae0b97f28d60545b34fcd
Yusuke <y-maeda@recus-groove.com>
2022-12-23 11:17:56 +0900

    remove comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,4 @@
-// import { addDecorator } from '@storybook/html';
-// import { withA11y } from '@storybook/addon-a11y'; //全てのコンポーネントで反映させるため、ここでimport
 // import '../src/scss/css/bundle.scss'; // グローバルスタイル
-
-// addDecorator(withA11y);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
