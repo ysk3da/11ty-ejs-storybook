c074a75de0a4eb256c481dc96b2e003aafdd329f
Yusuke <y-maeda@recus-groove.com>
2022-12-26 12:57:54 +0900

    format code

--- _templates/generator/components/prompt.js
+++ _templates/generator/components/prompt.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-      message: 'What is the name of component?',
-      name: 'name',
-      type: 'input',
-      validate: (answer) => {
-          if (answer !== '') {
-              return true;
-          }
-      },
+    message: "What is the name of component?",
+    name: "name",
+    type: "input",
+    validate: (answer) => {
+      if (answer !== "") {
+        return true;
+      }
+    },
   },
 ];
