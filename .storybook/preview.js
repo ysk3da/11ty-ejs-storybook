3a7305c1b53702d673400e9808d474a8397b0f72
Yusuke <y-maeda@recus-groove.com>
2022-12-26 11:57:01 +0900

    add easing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,25 @@
 import '../src/styles/global.scss'; // グローバルスタイル
 
+// ビューポートの追加
+import { MINIMAL_VIEWPORTS} from '@storybook/addon-viewport';
+
+const customViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -13,5 +33,11 @@ export const parameters = {
     config: {},
     options: {},
     manual: true,
+  },
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports,
+    },
   },
 }
