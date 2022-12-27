29fb92b3dd85a4fefae1179fd98a9af890ead565
Yusuke <y-maeda@recus-groove.com>
2022-12-27 10:07:14 +0900

    add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,50 @@
+const path = require("path");
+const pluginRev = require("eleventy-plugin-rev");
+const eleventySass = require("eleventy-sass");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+
 module.exports = function (eleventyConfig) {
   // Add a filter using the Config API
   // eleventyConfig.addTransform('htmlmin', function (content, outputPath) {})
+
+  eleventyConfig.addPlugin(pluginRev);
+  eleventyConfig.addPlugin(eleventySass, [
+    {
+      postcss: postcss([autoprefixer]),
+    },
+    {
+      // revision は ejs だと render時にcofig情報をコンパイルしないので諦め
+      rev: false,
+    },
+    {
+      compileOptions: {
+        permalink: function(contents, inputPath) {
+          let parsed = path.parse(inputPath);
+          // console.log({parsed});
+          if(parsed.name.startsWith("_")) {
+            return false;
+          }
+          return (data) => {
+            // console.log({data});
+            return data.page.filePathStem.replace(/^\/styles\//, "/css/") + ".css";
+          };
+        }
+      }
+    },
+  ]);
+
+  // Copy `src/img/` to `dist/img`
+  eleventyConfig.addPassthroughCopy("src/img");
+  // Copy `src/styles/` to `dist/css`
+  // eleventyConfig.addPassthroughCopy({"src/styles": "css"});
+  // Copy `src/scripts/` to `dist/js`
+  eleventyConfig.addPassthroughCopy({"src/scripts": "js"});
 
   // ejs offcial for customize
   let ejs = require("ejs");
   eleventyConfig.setLibrary("ejs", ejs);
 
-  // Copy `src/img/` to `dist/img`
-  eleventyConfig.addPassthroughCopy("src/img");
-  // Copy `src/styles/` to `dist/css`
-  eleventyConfig.addPassthroughCopy({"src/styles": "css"});
-  // Copy `src/scripts/` to `dist/js`
-  eleventyConfig.addPassthroughCopy({"src/scripts": "js"});
 
   // You can return your Config object (optional).
   return {
