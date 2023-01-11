cc4154143c6b4ca1d0bf03e03c5b74b1f29c60f0
Yusuke <y-maeda@recus-groove.com>
2023-01-11 16:14:28 +0900

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // plugins: [
-  //   require('postcss-import')(),
-  //   require('postcss-simple-vars')(),
-  //   require('autoprefixer')(),
-  //   require('cssnano')({
-  //     autoprefixer: false
-  //   })
-  // ]
+  plugins: [
+    require('autoprefixer')(),
+    // require('postcss-import')(),
+    // require('postcss-simple-vars')(),
+    // require('cssnano')({
+    //   autoprefixer: false
+    // })
+  ]
 }
