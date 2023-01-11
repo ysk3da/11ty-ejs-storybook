da7e072d71e47443ee7a653983d6fa2c2e22e63b
Yusuke <y-maeda@recus-groove.com>
2023-01-11 16:06:26 +0900

    パスを修正

--- _templates/generator/pages/index.js
+++ _templates/generator/pages/index.js
@@ -27,7 +27,7 @@ module.exports = {
       .prompt(questions)
       .then(answers => {
         if(answers.have_style){
-          indexScss += `\n@forward '../../_includes/pages/${answers.page_name}/page-${changeCase.param(answers.page_name)}';`;
+          indexScss += `\n@forward '../../pages/${answers.page_name}/page-${changeCase.param(answers.page_name)}';`;
           fs.writeFileSync(indexPath, indexScss, (err) => {
             if (err) throw err;
             console.log('pages _index.scss success');
