885c1077e0fa9fd7c74c999ed930a75a138b9460
Yusuke <y-maeda@recus-groove.com>
2023-02-08 13:24:59 +0900

    add story

--- src/_includes/components/Image/Image.stories.js
+++ src/_includes/components/Image/Image.stories.js
@@ -7,7 +7,14 @@ export default {
   title: 'Components/Images',
   component: ImageTemplate,
   argTypes: {
-    label: { control: 'text' },
+    src: "path/to/imgFile",
+    alt: "altText",
+    width: 100, // no unit integer
+    height: 100, // no unit integer
+    loading: "eager | lazy",
+    decoding: "sync | async | auto",
+    srcset: "path/to/imgFile 100w, path/to/imgFile-200 200w", // 解像度に合わせた画像へのパスとそのときのレンダリングサイズを 整数 + w で記述
+    sizes: "(max-width: 600px) 100px, 50vw", // 上記、srcset に対応したレンダリングサイズを指定
   },
   parameters: {
     notes: { markdown } // markdown 読み込み
